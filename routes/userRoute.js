07c5d1e822767c2ac04e77416c300e565dc00642
renuka-deshmukh <renuka.deshmukh776@gmail.com>
2025-11-15 16:13:22 +0530

    updated

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,7 +4,7 @@ const { protect } = require('../middleware/auth')
 const { uploadSingle } = require('../middleware/multer')
 const router = express.Router()
 
-router.post('/register' ,userController.register)
+router.post('/register' , uploadSingle('avatar'), userController.register)
 router.post('/login' , userController.login)
 router.get('/getUserInfo', protect, userController.getUserInfo)
 router.get('/getAllUsers', userController.getAllUsers)
